f0300484ca727d46309b1e1fd3d73168b45c66c5
Hong Diep <109153283+hngdiep23@users.noreply.github.com>
2024-03-03 07:29:32 +0700

    Time: 55 ms (53.84%), Space: 16.5 MB (76.08%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        m = len(str_x)
+        lst = []
+        for i in range(m):
+            idx = str_x[i]
+            lst.append(idx)
+        new_str = ''
+        for i in lst[::-1]:
+            new_str += i        
+        return new_str == str_x
